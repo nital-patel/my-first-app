9ca64126f759aa72bf4033475c2aadcfce36ebde
Nital Patel <patelnital36@gmail.com>
2019-03-16 18:25:34 -0400

    Add star component in under ngOnChanges method

---
+++ src/app/products/shared/star.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnChanges, Input } from '@angular/core';
+
+@Component ({
+  selector: 'app-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css']
+})
+export class StarComponent implements OnChanges {
+  @Input() rating: 4;
+  starWidth: number;
+  ngOnChanges(): void {
+    this.starWidth = this.rating * 75 / 5;
+  }
+}
