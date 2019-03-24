6d7c4eeeafe411afc8b0feb7ff8f70365b2d0124
Nital Patel <patelnital36@gmail.com>
2019-03-23 22:19:24 -0400

    Add product detail under product details

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import { IProduct} from '../product';
 
 @Component({
@@ -9,9 +10,22 @@ import { IProduct} from '../product';
 export class ProductDetailComponent implements OnInit {
   pageTitle: 'Product Detail';
   product: IProduct;
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `:${id}`;
+    this.product = {
+      productId: id,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March 19, 2016',
+      description: 'Leaf rake with 48-inch wooden handle.',
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl: 'https://tse3.mm.bing.net/th?id=OIP.QZDOqe-3GtduDpvzL6T_ggHaHa&pid=15.1&P=0&w=300&h=300'
+
+    };
   }
 
 }
